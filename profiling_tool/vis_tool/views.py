e931142aebe940eeb78cd4e67b3ae338248c742f
Rudolfs Petrovs <rudolfs.petrovs@gmail.com>
2019-05-06 23:18:12 +0100

    Columns selection feature

--- profiling_tool/vis_tool/views.py
+++ profiling_tool/vis_tool/views.py
@@ -10,10 +10,16 @@ class TimeSeriesView(PandasSimpleView):
     def get_data(self, request, *args, **kwargs):
         dataframe = request.GET.get("dataframe", None)
         if dataframe is not None and dataframe.lower() in {"cas" "acc", "veh"}:
-            data = 'vis_tool/data/{}.csv'.format(dataframe.capitalize())
+            filepath = 'vis_tool/data/{}.csv'.format(dataframe.capitalize())
         else:
-            data = 'vis_tool/data/Cas.csv'
-        return pd.read_csv(data, dtype={'Accident_Index': str})
+            filepath = 'vis_tool/data/Cas.csv'
+        df = pd.read_csv(filepath, dtype={'Accident_Index': str})
+        columns = request.GET.get("columns", None)
+        if columns is not None and all([x in df.columns for x in columns.split(",")]):
+            result_df = df[columns.split(",")]
+        else:
+            result_df = df
+        return result_df
 
     renderer_classes = [PandasHTMLRenderer]
     template_name = 'vis_tool/index.html'

251f1fda963209a8ce4b8150bfee4595351a80a2
Rudolfs Petrovs <rudolfs.petrovs@gmail.com>
2019-05-06 20:08:13 +0100

    Added first test for the page

--- profiling_tool/vis_tool/tests.py
+++ profiling_tool/vis_tool/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+
+class IndexViewTests(TestCase):
+    def test_index(self):
+        """
+        Check if page accessible.
+        """
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)

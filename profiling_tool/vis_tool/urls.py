3e0282b8c4e283fff10da5310b903c781fafff6e
Rudolfs Petrovs <rudolfs.petrovs@gmail.com>
2019-05-06 22:06:11 +0100

    Added initial data rendering as table.

--- profiling_tool/vis_tool/urls.py
+++ profiling_tool/vis_tool/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from . import views
-
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.TimeSeriesView.as_view(), name="index")
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- profiling_tool/vis_tool/views.py
+++ profiling_tool/vis_tool/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render, HttpResponse
+import pandas as pd
+from django.shortcuts import HttpResponse
+from rest_pandas import PandasSimpleView, PandasView
+from rest_pandas.renderers import PandasHTMLRenderer, PandasCSVRenderer
 
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("Test")
+class TimeSeriesView(PandasSimpleView):
+    def get_data(self, request, *args, **kwargs):
+        return pd.read_csv('vis_tool/data/Cas.csv', dtype={'Accident_Index': str})
+
+    renderer_classes = [PandasHTMLRenderer]
+    template_name = 'vis_tool/index.html'
